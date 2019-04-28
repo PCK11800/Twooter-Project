fce9f79a95c7b4ade74bd997d9a7dbc1d3447b13
PCK11800 <32240017+PCK11800@users.noreply.github.com>
2019-04-28 22:08:29 +0100

    Add files via upload

--- Messages_Panel.java
+++ Messages_Panel.java
@@ -1,5 +1,7 @@
 import twooter.*;
 import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
 import java.awt.Color;
 
 
@@ -10,6 +12,7 @@ public class Messages_Panel extends App{
     private static JLabel WRITE_MESSAGE_LABEL = new JLabel();
     private static JTextArea WRITE_MESSAGE_AREA = new JTextArea();
     private static JScrollPane WRITE_MESSAGE_AREA_SCROLLPANE = new JScrollPane(WRITE_MESSAGE_AREA, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    private static JButton SEND_MESSAGE_BUTTON = new JButton("Twoot");
     
     //Bools
     
@@ -42,12 +45,38 @@ public class Messages_Panel extends App{
         //WRITE_MESSAGE_AREA
         mainFrame.add(WRITE_MESSAGE_AREA_SCROLLPANE);
         WRITE_MESSAGE_AREA_SCROLLPANE.setBounds(WRITE_MESSAGE_AREA_X_POSITION, WRITE_MESSAGE_AREA_Y_POSITION, WRITE_MESSAGE_AREA_WIDTH, WRITE_MESSAGE_AREA_HEIGHT);
+        WRITE_MESSAGE_AREA.setText("FIX THIS DAMN BUG");
+        WRITE_MESSAGE_AREA.setText("");
         WRITE_MESSAGE_AREA.setWrapStyleWord(true);
         WRITE_MESSAGE_AREA.setLineWrap(true);
+
+        //SEND_MESSAGE_BUTTON
+        MESSAGES_PANEL.add(SEND_MESSAGE_BUTTON);
+        SEND_MESSAGE_BUTTON.setBounds((WRITE_MESSAGE_AREA_X_POSITION - 8), (WRITE_MESSAGE_AREA_Y_POSITION + 100), 100, 20);
+        SEND_MESSAGE_BUTTON.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                SEND_MESSAGE_BUTTON_ACTION_LISTENER();
+            }
+        });
+        
+        //Refresh Specific Components - to fix overlap and flickering
+        REFRESH_SPECIFIC_COMPONENTS();
     }
 
     public void removeMessagesPanel(){
         mainFrame.remove(MESSAGES_PANEL);
+        mainFrame.remove(WRITE_MESSAGE_AREA_SCROLLPANE); //This is because I decide to keep WRITE_MESSAGE_AREA and MESSAGE_PANEL seperate
         mainFrame.revalidate();
+    }
+
+    private void REFRESH_SPECIFIC_COMPONENTS(){
+        WRITE_MESSAGE_AREA_SCROLLPANE.repaint();
+    }
+
+    //SEND_MESSAGE_BUTTON ACTIONLISTENER FUNCTION
+    private void SEND_MESSAGE_BUTTON_ACTION_LISTENER(){
+        System.out.println("SEND_MESSAGE_BUTTON_ACTION_LISTENER");
+
+        //DO THINGS
     }
 }
